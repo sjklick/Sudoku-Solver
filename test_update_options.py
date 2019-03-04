ea27e02831126393aa651695daaadd481ae5c2f7
Steven Joshua Klickermann <sjklickermann@gmail.com>
2019-03-03 23:50:49 -0500

    Implement and test update_puzzle
    Implemented a function to determine which numbers (1-9) are available
    for use in each square, based on the sudoku rules for rows, columns,
    and 3x3 block (no repeating number in a row, etc). Visually tested
    the results of the update_puzzle function with test_update_puzzle.py.

--- test_update_options.py
+++ test_update_options.py
@@ -8,20 +8,10 @@ puzzle = [['5', '3', '.', '.', '7', '.', '.', '.', '.'],
           ['.', '.', '.', '4', '1', '9', '.', '.', '5'],
           ['.', '.', '.', '.', '8', '.', '.', '7', '9']]
 
-solution = [['5', '3', '.', '.', '7', '.', '.', '.', '.'],
-            ['6', '.', '.', '1', '9', '5', '.', '.', '.'],
-            ['.', '9', '8', '.', '.', '.', '.', '6', '.'],
-            ['8', '.', '.', '.', '6', '.', '.', '.', '3'],
-            ['4', '.', '.', '8', '.', '3', '.', '.', '1'],
-            ['7', '.', '.', '.', '2', '.', '.', '.', '6'],
-            ['.', '6', '.', '.', '.', '.', '2', '8', '.'],
-            ['.', '.', '.', '4', '1', '9', '.', '.', '5'],
-            ['.', '.', '.', '.', '8', '.', '.', '7', '9']]
-
-row_options = [[True for options in range(9)] for rows in range(9)]
-column_options = [[True for options in range(9)] for columns in range(9)]
-block_options = [[[True for options in range(9)] for y in range(3)] for x in range(3)]
-square_options = [[[True for options in range(9)] for row in range(9)] for column in range(9)]
+row_options = [[True for option in range(9)] for row in range(9)]
+column_options = [[True for option in range(9)] for column in range(9)]
+block_options = [[[True for option in range(9)] for x in range(3)] for y in range(3)]
+square_options = [[[True for option in range(9)] for column in range(9)] for row in range(9)]
 
 def print_puzzle(puzzle):
     for row in range(9):
@@ -69,21 +59,43 @@ def update_options(puzzle):
                     square_options[row][column][option] = False
                 square_options[row][column][int(puzzle[row][column])-1] = True
 
-def update_solution(puzzle):
-    for row in range(9):
-        for column in range(9):
-            num_options = 0
-            for option in range(9):
-                if square_options[row][column][option] == True:
-                    num_options += 1
-            if num_options == 1:
-                for option in range(9):
-                    if square_options[row][column][option] == True:
-                        puzzle[row][column] = option+1
-                        break
-
 print_puzzle(puzzle)
 print('')
 update_options(puzzle)
-update_solution(solution)
-print_puzzle(solution)
+# Check row options
+print('row_options:')
+for row in range(9):
+    print('row['+str(row)+']:', end=' ')
+    for option in range(9):
+        if row_options[row][option] == True:
+            print(str(option+1), end=' ')
+    print('')
+print('')
+# Check column options
+print('column_options:')
+for column in range(9):
+    print('column['+str(column)+']:', end=' ')
+    for option in range(9):
+        if column_options[column][option] == True:
+            print(str(option+1), end=' ')
+    print('')
+print('')
+# Check block options
+print('block options:')
+for block_y in range(3):
+    for block_x in range(3):
+        print('block['+str(block_y)+']['+str(block_x)+']:', end=' ')
+        for option in range(9):
+            if block_options[block_y][block_x][option] == True:
+                print(str(option+1), end=' ')
+        print('')
+print('')
+# Check square options
+print('square options:')
+for row in range(9):
+    for column in range(9):
+        print('square['+str(row)+']['+str(column)+'[:', end=' ')
+        for option in range(9):
+            if square_options[row][column][option] == True:
+                print(str(option+1), end= ' ')
+        print('')
