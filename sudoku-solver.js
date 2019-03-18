232592e29c330558043d31ba833e64d5a8b2d856
Steven Joshua Klickermann <sjklickermann@gmail.com>
2019-03-18 07:08:41 -0400

    Prevent editing of solved puzzle
    The puzzle grid is now disabled if the puzzle has been solved, and
    the value options buttons are hidden.

--- sudoku-solver.js
+++ sudoku-solver.js
@@ -384,13 +384,36 @@ function solve() {
     }
 }
 
+function disable_puzzle() {
+    var square, index, options, centering;
+    square = document.getElementsByClassName("sudoku-square");
+    for (index=0; index<9*9; index++) {
+        square[index].disabled = true;
+    }
+    options = document.getElementsByClassName("value-options")[0];
+    options.style.display = "none";
+    centering = document.getElementsByClassName("centering-div")[0];
+}
+
+function enable_puzzle() {
+    var square, index;
+    square = document.getElementsByClassName("sudoku-square");
+    for (index=0; index<9*9; index++) {
+        square[index].disabled = false;
+    }
+    options = document.getElementsByClassName("value-options")[0];
+    options.style.display = "grid";
+}
+
 function action_button_pressed() {
     var button;
     button = document.getElementById("clear-solve");
     if (button.innerText == "Clear") {
         clear_grid();
         button.innerText = "Solve";
+        enable_puzzle();
     } else {
+        disable_puzzle();
         solve();
         button.innerText = "Clear";
     }
